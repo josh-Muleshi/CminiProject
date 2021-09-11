a1e50054469cab8d623bd90cd5c971b5bdc222d2
josh-Muleshi <jmuleshi2@gmail.com>
2021-09-11 18:00:14 +0200

    finished the main's Ui

--- source/main.c
+++ source/main.c
@@ -17,15 +17,17 @@
 
 int main(){
 
-    printf("-------------------------------------------------");
-    printf("|               gestion des etudiants           |");
-    printf("-------------------------------------------------\n");
+    printf("\t-------------------------------------------------------------------\n");
+    printf("\t|                        gestion des etudiants                    |\n");
+    printf("\t-------------------------------------------------------------------\n\n");
 
     printf("\t bienvenu dans kalopeto une app pour la gestion des etudiants\n\n");
+    printf("\t Entrez le numero d'item pour effectuer l'action correspondente\n\n");
 
-    printf("\t\t")
+    printf("\t\t1. Enregistrement\n");
+    printf("\t\t2. Affichage des données\n");
+    printf("\t\t3. Quitter l’application\n\n");
 
-
-
+    printf("\t-------------------------------------------------------------------\n\n");
     return 0;
 }
