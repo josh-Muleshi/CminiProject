e274ad7b455021a0498dc8ff0e9487a9b9f59643
josh-Muleshi <jmuleshi2@gmail.com>
2021-09-11 13:52:50 +0200

    added obligatoire comments

--- source/main.c
+++ source/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/*
+* File name : main.c
+*
+* Description : le point d'entrer de notre programme
+*
+* Author :
+*
+* Cedric
+* Aaron
+* Grace
+* Gloire
+* josh
+*/
+
 int main(){
 
     printf("josh Muleshi");
