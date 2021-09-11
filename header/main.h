6bc6368bbd9c5edf84cd9958daa2131f691a1580
josh-Muleshi <jmuleshi2@gmail.com>
2021-09-11 15:22:34 +0200

    the main file initialised

---
+++ header/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/*typedef struct Personne
+{
+    char nom[50];
+    char postnom[50];
+    int age ;
+} Personne;*/
+
+#endif //MAIN_H_INCLUDED
--- source/main.c
+++ source/main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /*
 * File name : main.c
 *
@@ -12,11 +10,22 @@
 * Grace
 * Gloire
 * josh
+*
+* Package name : Kalopeto/source/main.c
 */
+#include <stdio.h>
 
 int main(){
 
-    printf("josh Muleshi");
+    printf("-------------------------------------------------");
+    printf("|               gestion des etudiants           |");
+    printf("-------------------------------------------------\n");
+
+    printf("\t bienvenu dans kalopeto une app pour la gestion des etudiants\n\n");
+
+    printf("\t\t")
+
+
 
     return 0;
 }
